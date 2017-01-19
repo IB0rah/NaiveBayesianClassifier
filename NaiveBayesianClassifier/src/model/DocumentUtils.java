7ae2438df7d79a7fb217ed7b539613c889c18ae9
Vincent <vincent.dunning@gmail.com>
2017-01-19 11:31:47 +0100

    tokenizer

--- NaiveBayesianClassifier/src/model/DocumentUtils.java
+++ NaiveBayesianClassifier/src/model/DocumentUtils.java
@@ -4,10 +4,17 @@ import java.util.*;
 
 public class DocumentUtils {
 	
+	private static List<String> stopwords = new ArrayList<String>();
+	
 	public static List<String> tokenize(Document doc) {
 		List<String> result = new ArrayList<String>();
 		for(String word: doc.getWords()) {
 			String lowercased = word.toLowerCase();
+			if(!stopwords.contains(lowercased)) {
+				result.add(lowercased);
+			}
 		}
+		
+		return result;
 	}
 }
