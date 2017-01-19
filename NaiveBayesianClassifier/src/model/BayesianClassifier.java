476ffe7110bf9cfad1f09127529e90513fcf8497
Vincent <vincent.dunning@gmail.com>
2017-01-19 10:52:22 +0100

    Backend almost done

--- NaiveBayesianClassifier/src/model/BayesianClassifier.java
+++ NaiveBayesianClassifier/src/model/BayesianClassifier.java
@@ -1,10 +1,79 @@
 package model;
 
+import java.util.*;
 public class BayesianClassifier {
 	
-	private int totalWordCount = 0;
+	private List<String> vocabulary = new ArrayList<String>();
+	private Map<Class, Double> classes = new HashMap<Class, Double>();
+	private int documentCount = 0;
 	
-	public int getTotalWordCount() {
-		return totalWordCount;
+	public BayesianClassifier() {
+		
+	}
+	
+	public int getVocabularySize() {
+		return vocabulary.size();
+	}
+	
+	public void train(Map<Class, List<Document>> trainingsData) {
+		List<String> vocabulary = extractVocabulary(trainingsData);
+		int docsCount = CountNumberOfDocs(trainingsData);
+		for(Class c: trainingsData.keySet()) {
+			double classPrior = trainingsData.get(c).size() / docsCount;
+			classes.put(c, classPrior);
+			c.train(trainingsData.get(c));
+		}
+		
+		this.vocabulary = vocabulary;
+		this.documentCount = docsCount;
+	}
+	
+	public int CountNumberOfDocs(Map<Class, List<Document>> trainingsData) {
+		int result = 0;
+		for(Class c: trainingsData.keySet()) {
+			int docsCount = trainingsData.get(c).size();
+			result += docsCount;
+		}
+		return result;
+	}
+	
+	public List<String> extractVocabulary(Map<Class, List<Document>> trainingsData) {
+		List<String> result = new ArrayList<String>();
+		for(Class c: trainingsData.keySet()) {
+			List<Document> documents = trainingsData.get(c);
+			for(Document doc: documents) {
+				List<String> words = doc.getWords();
+				for(String w: words) {
+					if(!result.contains(w)) {
+						result.add(w);
+					}
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	public Class classify(Document doc) {
+		Class result = null;
+		for(Class c: classes.keySet()) {
+			double ClassProbabilty = classes.get(c) + c.getDocumentConditionalProbability(doc);
+		}
+		
+		return result;
+	}
+	
+	public void train(Document document, Class c) {
+		double oldClassPrior = classes.get(c);
+		double oldNrOfDocumentsClass = oldClassPrior * documentCount;
+		documentCount++;
+		for(String word: document.getWords()) {
+			if(!vocabulary.contains(word)) {
+				vocabulary.add(word);
+			}
+		}
+		classes.put(c, oldNrOfDocumentsClass + 1 / documentCount);
+		c.train(document);
+		
 	}
 }
--- NaiveBayesianClassifier/src/model/Class.java
+++ NaiveBayesianClassifier/src/model/Class.java
@@ -3,10 +3,24 @@ package model;
 import java.util.*;
 
 public class Class {
-	
+	public static final int SMOOTHING = 1;
 	//map of all words that belong to this class + their frequencies
 	Map<String, Integer> bagOfWords = new HashMap<String, Integer>();
+	Map<String, Double> conditionalProbabilities = new HashMap<String, Double>();
+	private int totalNumberOfWords = 0;
+	private BayesianClassifier bc;
+	private String name;
 	
+	public Class(String name, BayesianClassifier classifier) {
+		this.name = name;
+		this.bc = classifier;
+	}
+	
+	public void train(List<Document> documents) {
+		for(Document doc: documents) {
+			this.train(doc);
+		}
+	}
 	public void train(Document document) {
 		for(String word: document.getWords()) {
 			putWord(word);
@@ -15,10 +29,34 @@ public class Class {
 	
 	//Add a word
 	public void putWord(String word) {
+		totalNumberOfWords++;
 		if(bagOfWords.containsKey(word)) {
 			bagOfWords.put(word, bagOfWords.get(word) + 1);
 		} else {
 			bagOfWords.put(word, 1);
 		}
+		double conditionalProbability = (bagOfWords.get(word) + SMOOTHING) /(totalNumberOfWords + SMOOTHING * bc.getVocabularySize());   
+		conditionalProbabilities.put(word, conditionalProbability );
+	}
+	
+	public double getDocumentConditionalProbability(Document doc) {
+		double score = 0;
+		for(String word : doc.getWords()) {
+			score =  score * getWordConditionalProbability(word);
+		}
+		return score;
+	}
+	
+	public double getWordConditionalProbability(String word) {
+		double score;
+		if(conditionalProbabilities.containsKey(word)) {
+			score = (bagOfWords.get(word) + SMOOTHING) /(totalNumberOfWords + SMOOTHING * bc.getVocabularySize());
+		} else {
+			score = (0 + SMOOTHING) /(totalNumberOfWords + SMOOTHING * bc.getVocabularySize());
+		}
+		return score;
+	}
+	public String getName() {
+		return name;
 	}
 }
